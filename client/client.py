48320215f9fea9041a87073a743e649adfd19826
Mohammed Salah <36507806+Salah1174@users.noreply.github.com>
2024-12-03 18:15:18 +0200

    Server,Client socket code

---
+++ client/client.py
@@ -0,0 +1,8 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 8080))  # Use localhost
+client.send("I am Salah\n".encode())
+from_server = client.recv(4096)
+client.close()
+print(from_server.decode())
