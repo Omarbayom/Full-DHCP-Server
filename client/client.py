827ff0ceee37fbb124c0d2ad87270ee2d3a159af
Seif Yasser <21p0102@eng.asu.edu.eg>
2024-12-06 13:53:28 +0200

    Implement DHCP client and server functionality with UDP communication

--- client/client.py
+++ client/client.py
@@ -1,8 +1,43 @@
 import socket
+from config import SERVER_PORT, CLIENT_PORT, BUFFER_SIZE
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 8080))  # Use localhost
-client.send("I am Salah\n".encode())
-from_server = client.recv(4096)
-client.close()
-print(from_server.decode())
+SERVER_IP = "127.0.0.1"
+
+
+def dhcp_client():
+    # Create a UDP socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket.bind(("", CLIENT_PORT))
+
+    try:
+        # Send DHCPDISCOVER
+        discover_message = b"DHCPDISCOVER"
+        client_socket.sendto(discover_message, (SERVER_IP, SERVER_PORT))
+        print("Sent DHCPDISCOVER")
+
+        # Wait for DHCPOFFER
+        data, server = client_socket.recvfrom(BUFFER_SIZE)
+        print(f"Received from server: {data}")
+
+        if data.startswith(b"DHCPOFFER:"):
+            offered_ip = data.split(b":")[1].decode()
+            print(f"Offered IP: {offered_ip}")
+
+            # Send DHCPREQUEST
+            request_message = b"DHCPREQUEST"
+            client_socket.sendto(request_message, (SERVER_IP, SERVER_PORT))
+            print("Sent DHCPREQUEST")
+
+            # Wait for DHCPACK
+            data, server = client_socket.recvfrom(BUFFER_SIZE)
+            print(f"Received from server: {data}")
+
+            if data.startswith(b"DHCPACK:"):
+                assigned_ip = data.split(b":")[1].decode()
+                print(f"Assigned IP: {assigned_ip}")
+    finally:
+        client_socket.close()
+
+
+if __name__ == "__main__":
+    dhcp_client()
--- server/server.py
+++ server/server.py
@@ -1,18 +1,48 @@
 import socket
-serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv.bind(('127.0.0.1', 8080))
-serv.listen(5)
+from threading import Thread
+from config import SERVER_PORT, CLIENT_PORT, BUFFER_SIZE
+
+# IP pool for the server to assign
+ip_pool = ["192.168.1.2", "192.168.1.3", "192.168.1.4"]
+leased_ips = {}
 
 
-while True:
-  conn, addr = serv.accept()
-  from_client = ''
-  while True:
-    data = conn.recv(4096)
-    if not data:
-      break
-    from_client += data.decode('utf8')
-    print(f'From client: {from_client}')
-    conn.send("I am SERVER\n".encode())
-  conn.close()
-print('client disconnected and shutdown')
+def handle_client(data, addr, server_socket):
+    global ip_pool, leased_ips
+    print(f"Received data from {addr}: {data}")
+
+    if data.startswith(b"DHCPDISCOVER"):
+        if ip_pool:
+            offered_ip = ip_pool.pop(0)
+            leased_ips[addr] = offered_ip
+            offer_message = f"DHCPOFFER:{offered_ip}".encode()
+            server_socket.sendto(offer_message, (addr[0], CLIENT_PORT))
+            print(f"Offered IP {offered_ip} to {addr}")
+        else:
+            print("No available IPs to offer.")
+    elif data.startswith(b"DHCPREQUEST"):
+        client_ip = leased_ips.get(addr)
+        if client_ip:
+            ack_message = f"DHCPACK:{client_ip}".encode()
+            server_socket.sendto(ack_message, (addr[0], CLIENT_PORT))
+            print(f"Assigned IP {client_ip} to {addr}")
+        else:
+            print(f"No lease found for {addr}.")
+    else:
+        print("Unknown message type.")
+
+
+def start_server():
+    # Create a UDP socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind(("", SERVER_PORT))
+    print(f"DHCP Server is running on port {SERVER_PORT}...")
+
+    # Handle multiple clients
+    while True:
+        data, addr = server_socket.recvfrom(BUFFER_SIZE)
+        Thread(target=handle_client, args=(data, addr, server_socket)).start()
+
+
+if __name__ == "__main__":
+    start_server()
