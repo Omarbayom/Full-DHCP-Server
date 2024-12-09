e33e44814533e765c081acd2964f63c0a49fd556
Omar bayomi <omarbayom2601@gmail.com>
2024-12-09 20:34:48 +0200

    server ip update

--- bayom/server.py
+++ bayom/server.py
@@ -6,7 +6,7 @@ import time
 # Configuration
 ip_pool = ["192.168.1.100", "192.168.1.101", "192.168.1.102"]  # Example IP pool
 lease_duration = 5  # Lease time in seconds
-server_ip = "192.168.1.10"  # DHCP server IP address
+server_ip = socket.gethostbyname(socket.gethostname())
 lease_table = {}  # Tracks active leases: {client_address: (ip, lease_expiry)}
 
 # Lock for synchronizing access to shared data
